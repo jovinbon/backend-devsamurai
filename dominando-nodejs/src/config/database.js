fb8d32ba886b6f877e6d40eba0e02135e19da73c
= <=>
2023-03-22 15:54:07 -0300

    add .gitignore

--- dominando-nodejs/src/config/database.js
+++ dominando-nodejs/src/config/database.js
@@ -2,7 +2,7 @@ module.exports = {
  dialect: "postgres",
  host: "localhost",
  username: "postgres",
- password: "postgres",
+ password: "123456",
  database: "teste-dominando-nodejs",
  define: {
   timestamp: true, // cria duas colunas: createdAt e updatedAt
